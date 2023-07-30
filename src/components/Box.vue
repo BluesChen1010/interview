7fe6fe9c36351fa8bb9d9680cfb31d694ce14415
Blues <blueschen@jetfuel.com.tw>
2023-07-31 01:01:59 +0800

    add  bonus  question

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -14,6 +14,13 @@ const showAnimate = (n, act) => {
   return false
 }
 
+const showCircle = (n, act) => {
+  if (n === 1 || n === 3) {
+    return act === 1 || act === 3
+  }
+  return false
+}
+
 const type = ref('一般模式')
 const changeType = () => {
   if (type.value === '一般模式') type.value = '特殊模式'
@@ -25,10 +32,18 @@ const changeType = () => {
 <template>
   <h1>{{ msg }}</h1>
   <button type="button" @click="changeType()">{{ type }}</button>
-  <div class="card">
+  <div class="normal-card" v-if="type === '一般模式'">
+    <p>題目: 方塊寬度200px ,題目動畫向右500px, 所以最左邊球體不會完全消失</p>
     <ul v-for="n in 3">
       <li :class="{'move': showAnimate(n, act)}" v-for="act in 3">
-        <div class="circle"></div>
+        <div v-if="showCircle(n, act)" class="circle normal"></div>
+      </li>
+    </ul>
+  </div>
+  <div class="special-card" v-if="type === '特殊模式'">
+    <ul v-for="n in 3">
+      <li :class="{'move': showAnimate(n, act)}" v-for="act in 3">
+        <div v-if="showCircle(n, act)"  class="circle special"></div>
       </li>
     </ul>
   </div>
@@ -38,15 +53,20 @@ const changeType = () => {
 .circle
   z-index: 2
   position: absolute
-  top: 40px
-  animation-name: circle
-  animation-duration: 7s
-  animation-iteration-count: infinite
   width: 30px
   height: 30px
   border-radius: 50%
   background-color: #A5F12B
-.card
+  &.normal
+    animation-duration: 7s
+    animation-iteration-count: infinite
+    animation-name: circle
+  &.special
+    animation-name: moveTo
+    animation-duration: 5s
+    animation-iteration-count: infinite
+
+.normal-card
   padding: 0
   overflow: hidden
   position: relative
@@ -64,6 +84,9 @@ const changeType = () => {
     align-items: center
     li
       position: relative
+      display: flex
+      justify-content: center
+      align-items: center
       margin: 5px
       width: 200px
       height: 100px
@@ -74,12 +97,70 @@ const changeType = () => {
         animation-name: opacityColor
         animation-duration: 2s
         animation-iteration-count: infinite
+.special-card
+  padding: 0
+  overflow: hidden
+  position: relative
+  display: flex
+  justify-items: center
+  align-items: center
+  flex-direction: column
+  ul
+    height: auto
+    width: auto
+    margin: 0
+    padding: 0
+    display: flex
+    justify-items: center
+    align-items: center
+    li
+      display: flex
+      justify-content: center
+      align-items: center
+      margin: 5px
+      width: 200px
+      height: 100px
+      border: black solid 2px
+      background: radial-gradient(circle, rgba(113,81,95,1) 81%, rgba(0,0,0,1) 100%)
+      list-style: none
+    &:nth-child(1)
+      li
+        &:nth-child(1)
+          .circle
+            right: 515px
+            bottom: 265px
+        &:nth-child(3)
+          .circle
+            right: 90px
+            bottom: 265px
+    &:nth-child(3)
+      li
+        &:nth-child(1)
+          .circle
+            right: 515px
+            bottom: 40px
+        &:nth-child(3)
+          .circle
+            right: 90px
+            bottom: 40px
+
+        
+      &.move
+        animation-name: opacityColor
+        animation-duration: 2s
+        animation-iteration-count: infinite
 
 @keyframes circle
   from
-    left: 0
+    left: 100px
   to
-    left: 700px
+    left: 500px
+
+
+@keyframes moveTo
+  to
+    right: 0px
+    bottom: 0px
 
 
 @keyframes opacityColor
