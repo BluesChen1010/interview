cc3e49dbcee1b0e194647edaf6266a64d165ff04
Blues <blueschen@jetfuel.com.tw>
2023-07-30 11:24:09 +0800

    circle and box layout css

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Box from './components/Box.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Box msg="Purple 520" />
 </template>
 
 <style scoped>
---
+++ src/components/Box.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <button type="button" @click="count++">次數: {{ count }}</button>
+  <div class="card">
+    <ul v-for="n in 3">
+      <li v-for="n in 3"></li>
+    </ul>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.circle
+  width: 30px
+  height: 30px
+  background-color: #A5F12B
+.card
+  position: relative
+  display: flex
+  justify-items: center
+  align-items: center
+  flex-direction: column
+  ul
+    margin: 0
+    display: flex
+    justify-items: center
+    align-items: center
+    li
+      margin: 5px
+      width: 200px
+      height: 100px
+      border: black solid 2px
+      background: radial-gradient(circle, rgba(113,81,95,1) 81%, rgba(0,0,0,1) 100%)
+      list-style: none
+</style>
